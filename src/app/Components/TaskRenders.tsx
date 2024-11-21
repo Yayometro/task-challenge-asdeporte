6c2fc0bb14f497a521aaddf83ef57267af3143a6
Jair Vazquez <gsfrido10@outlook.com>
2024-11-20 18:15:27 -0600

    adding separation in dones and not dones

--- src/app/Components/TaskRenders.tsx
+++ src/app/Components/TaskRenders.tsx
@@ -14,14 +14,30 @@ function TaskRenders({onEdition}:props) {
     const taskGlobalState = useAppSelector(state => state.task)
     
   return (
-    <div className='w-full h-full px-2 flex flex-col gap-2 pt-5 items-center justify-start'>
+    <div className='w-full  px-2 flex flex-col gap-2 pt-5 items-center justify-start'>
         {
             !taskGlobalState || taskGlobalState.length === 0 ? (
                 <div className='text-center p-3'>{"No task to display, create one!" }</div>
             ): (
-                taskGlobalState.map(t => (
-                    <Task task={t} key={t.id} onEdition={onEdition}/>
-                ))
+                <div className="w-full h-full">
+                    <div className="undone flex flex-col gap-2 items-center justify-center">
+                    <p>To do:</p>
+                        {
+                            taskGlobalState.filter(t => !t.active).map(t => (
+                                <Task task={t} key={t.id} onEdition={onEdition}/>
+                            ))
+                        }
+                    </div>
+                    <div className="done flex flex-col gap-2 items-center justify-center">
+                        <p>Checked:</p>
+                        {
+                            taskGlobalState.filter(t => t.active).map(t => (
+                                <Task task={t} key={t.id} onEdition={onEdition}/>
+                            ))
+                        }
+                    </div>
+                </div>
+                
             )
         }
     </div>
